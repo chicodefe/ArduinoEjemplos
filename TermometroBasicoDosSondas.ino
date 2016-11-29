360545a1c7e07e0281cbf16ab6ee76d02ed23b9c
chicodefe <chicodefe@gmail.com>
2016-11-29 11:07:26 -0600

    Add files via upload

--- TermometroBasicoDosSondas.ino
+++ TermometroBasicoDosSondas.ino
@@ -3,48 +3,59 @@
  Created:	11/28/2016 12:59:59 AM
  Author:	Chicodefe
 */
+
 //Librerias para Serial
 #include <SoftwareSerial.h>
 #include <HardwareSerial.h>
-
-//Librerias para sensor de temperatura
-#include <OneWire.h>
-#include <DallasTemperature.h>
 
 //Librerias para Display
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3,POSITIVE); // Set the LCD I2C address
-OneWire oneWire_in(2); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire_out(3); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+//Librerias para sensor de temperatura
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+//Parametros Display
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3,POSITIVE); // Set the LCD I2C address
+
+//Parametros Libreria OneWire
+OneWire oneWire_in(4); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire_out(5); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+
+//Parametros Libreria DallasTemperature
 DallasTemperature sensor_in(&oneWire_in);// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensor_out(&oneWire_out);// Pass our oneWire reference to Dallas Temperature.
 
-
 void setup()// the setup function runs once when you press reset or power the board
 	{
-	pinMode(2,INPUT); // sets the digital pin 2 as input
-	pinMode(3,INPUT); // sets the digital pin 3 as input
+
+	//Parametros Pines
+	pinMode(4,INPUT); // sets the digital pin 4 as input
+	pinMode(5,INPUT); // sets the digital pin 5 as input
+
+	//Parametros Serial
 	Serial.begin(9600);// initialize serial communication at 9600 bits per second
+
+	//Parametros LCD
 	lcd.begin(16,2); // initialize the lcd for 16 chars 2 lines, turn on backlight
 	lcd.setCursor(0,0);
-	lcd.print("Iniciando");
+	lcd.print("Iniciando...");
 	delay(300);
 	lcd.setCursor(0,1);
 	lcd.print("Listo");
 	delay(300);
-	// Wait and then tell user they can start the Serial Monitor and type in characters to
-	// Display. (Set Serial Monitor option to "No Line Ending")
 	lcd.clear();
 	}// end of setup
 
 void loop()// the loop function runs over and over again until power down or reset
 	{
+
+	//Parametros Sensores
 	sensor_in.begin();// Iniciando el Sensor
 	sensor_out.begin();// Iniciando el Sensor
 
+	//Verificacion do Sensores
 	if(sensor_in.getDeviceCount()==0||sensor_out.getDeviceCount()==0)
 		{
 		Serial.println("Sensor no Conectado");
@@ -58,8 +69,10 @@ void loop()// the loop function runs over and over again until power down or res
 		return;
 		}
 
+	//Solicitudes de Temperatura
 	sensor_in.requestTemperatures();
 	sensor_out.requestTemperatures();
+
 	//Print on Serial
 	Serial.print("Temperatura In: ");
 	Serial.print(sensor_in.getTempCByIndex(0));
