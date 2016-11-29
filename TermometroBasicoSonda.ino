e6d69e92bb7c46d0e773a48d0342dcc0ade55062
chicodefe <chicodefe@gmail.com>
2016-11-28 23:16:11 -0600

    Add files via upload

--- TermometroBasicoSonda.ino
+++ TermometroBasicoSonda.ino
@@ -13,20 +13,18 @@ Author:	Chicodefe
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-OneWire oneWire_Pin(4);
+
 LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3,POSITIVE);// Set the LCD I2C address
-DallasTemperature sensorSonda(&oneWire_Pin);
+OneWire oneWire_Pin(2); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+DallasTemperature sensorSonda(&oneWire_Pin);// Pass our oneWire reference to Dallas Temperature.
+int numberOfDevices; // Number of temperature devices found
+DeviceAddress tempDeviceAddress; // We'll use this variable to store a found device address
+
 void setup()// the setup function runs once when you press reset or power the board
 	{
-	pinMode(4, INPUT);// sets the digital pin 4 as input
+	pinMode(2,INPUT);// sets the digital pin 4 as input
 	Serial.begin(9600);// initialize serial communication at 9600 bits per second
-	sensorSonda.begin();// Iniciando el Sensor
 	lcd.begin(16,2);// initialize the lcd for 16 chars 2 lines, turn on backlight
-	lcd.backlight();
-	delay(300);
-	lcd.noBacklight();
-	delay(300);
-	lcd.backlight();
 	lcd.setCursor(0,0);
 	lcd.print("Iniciando");
 	delay(300);
@@ -40,6 +38,21 @@ void setup()// the setup function runs once when you press reset or power the bo
 	}// end of setup
 void loop()// the loop function runs over and over again until power down or reset
 	{
+	sensorSonda.begin();// Iniciando el Sensor
+	//if(!sensorSonda.getAddress(insideThermometer,0)) Serial.println("Unable to find address for Device 0");
+	if(sensorSonda.getDeviceCount()==0)
+		{
+		/*lcd.clear();*/
+		Serial.println("Sensor no Conectado");
+		lcd.setCursor(0,0); //Start at character 0 on line 0
+		lcd.print("Sensor ");
+		lcd.setCursor(0,1);//Start at character 0 on line 2
+		lcd.print("no Conectado");
+		delay(1000);
+		lcd.clear();
+		delay(100);
+		return; }
+	
 	sensorSonda.requestTemperatures();
 	Serial.print("Temperatura: ");
 	Serial.print(sensorSonda.getTempCByIndex(0));
@@ -53,5 +66,7 @@ void loop()// the loop function runs over and over again until power down or res
 	lcd.print((char)223);
 	lcd.print(" Celsius");
 	delay(10000);
-	lcd.clear();
+	lcd.setCursor(0,1);//Start at character 0 on line 2
+	lcd.print("                ");
+	//lcd.clear();
 	}// end of for loop
