9c59cb5b9af0981d5159224fd0e40ab17da52766
chicodefe <chicodefe@gmail.com>
2016-12-02 10:07:44 -0600

    Add files via upload

--- SensorBasicoFlujo.ino
+++ SensorBasicoFlujo.ino
@@ -4,14 +4,25 @@ Created:	11/27/2016 6:17:42 PM
 Author:	Chicodefe
 */
 
-volatile int NumPulsos; //variable para la cantidad de pulsos recibidos
+//Librerias para Serial
+#include <SoftwareSerial.h>
+#include <HardwareSerial.h>
+
+//Librerias para Display
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+//Parametros Display
+LiquidCrystal_I2C lcd(0x3f,2,1,0,4,5,6,7,3,POSITIVE); // Set the LCD I2C address
+
+volatile long NumPulsos; //variable para la cantidad de pulsos recibidos
 int PinSensor=2;    //Sensor conectado en el pin 2
+int tiempoReset=0;
 
 void ContarPulsos()//---Función que se ejecuta en interrupción
 	{
 	NumPulsos++;  //incrementamos la variable de pulsos
 	}
-
 
 int ObtenerFrecuencia()//---Función para obtener frecuencia de los pulsos
 	{
@@ -24,26 +35,42 @@ int ObtenerFrecuencia()//---Función para obtener frecuencia de los pulsos
 	return frecuencia;
 	}
 
-void setup()
+void setup()// the setup function runs once when you press reset or power the board
 	{
-	Serial.begin(9600);
-	pinMode(PinSensor,INPUT);
-	attachInterrupt(0,ContarPulsos,RISING); //(Interrupcion 0(Pin2),funcion,Flanco de subida)
+
+	//Parametros Pines
+	pinMode(PinSensor,INPUT); // sets the digital pin 2 as input
+
+	//Parametros Serial
+	Serial.begin(9600);// initialize serial communication at 9600 bits per second
+
+	attachInterrupt(digitalPinToInterrupt(2),ContarPulsos,CHANGE); //(Interrupcion 0(Pin2),funcion,Flanco de subida)
 	}
 
-void loop()
+void loop()// the loop function runs over and over again until power down or reset
 	{
 	int frecuencia=ObtenerFrecuencia(); //obtenemos la Frecuencia de los pulsos en Hz
 
-	Serial.print("FrecuenciaPulsos: ");//-----Enviamos por el puerto serie---------------
+
+	Serial.print("Pulsos por Segundo: ");//-----Enviamos por el puerto serie---------------
 	Serial.println(frecuencia);
-	if(frecuencia==0)
+
+	if(frecuencia!=0)
 		{
-		Serial.println("Inactivo");
+		tiempoReset=5;
+		}
+	else if(frecuencia==0&&tiempoReset>0)
+		{
+		tiempoReset=--tiempoReset;
+		}
+
+	if(tiempoReset>0)
+		{
+		Serial.print("Activado ");
+		Serial.println(tiempoReset);
 		}
 	else
-		{
-		Serial.println("Activado");
-		}
+		{Serial.println("Inactivo ");
+	
 	}
-
+	}
