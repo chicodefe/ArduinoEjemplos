c28bf30c7bc4f1642c9338621a0c316f9560461c
chicodefe <chicodefe@gmail.com>
2016-12-28 19:56:16 -0600

    Add files via upload

--- EsperarSinDelay.ino
+++ EsperarSinDelay.ino
@@ -24,49 +24,49 @@ http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
 #include <SoftwareSerial.h>
 #include <HardwareSerial.h>
 
-int ledState=LOW;// ledState used to set the LED
+int ledState = LOW;// ledState used to set the LED
 
 // Generally, you should use "unsigned long" for variables that hold time. The value will quickly become too large for an int to store
-unsigned long previousMillis1=0;// millis() returns an unsigned long.
-unsigned long intervalMillis1=100; // the time we need to wait
+unsigned long previousMillis1 = 0;// millis() returns an unsigned long.
+unsigned long intervalMillis1 = 100; // the time we need to wait
 unsigned long time;
 
 void setup()// the setup function runs once when you press reset or power the board
-	{
+{
 	//Parametros Pines
-	pinMode(13,OUTPUT);// sets the digital pin 4 as input
+	pinMode(13, OUTPUT);// sets the digital pin 4 as input
 
 	//Parametros Serial
 	Serial.begin(9600);// initialize serial communication at 9600 bits per second
-	}
+}
 
 void loop()// the loop function runs over and over again until power down or reset
-	{
+{
 	//Asignar el tiempo desde transcurrido
-	unsigned long currentMillis=millis();
+	unsigned long currentMillis = millis();
 
-	if(currentMillis-previousMillis1>=intervalMillis1)
-		{
+	if (currentMillis - previousMillis1 >= intervalMillis1)
+	{
 		// save the last time you blinked the LED
-		previousMillis1=currentMillis;
+		previousMillis1 = currentMillis;
 
 		// if the LED is off turn it on and vice-versa:
-		if(ledState==LOW)
-			{
-			ledState=HIGH;
-			}
+		if (ledState == LOW)
+		{
+			ledState = HIGH;
+		}
 		else
-			{
-			ledState=LOW;
-			}
+		{
+			ledState = LOW;
+		}
 
 		// set the LED with the ledState of the variable:
-		digitalWrite(13,ledState);
-		}
+		digitalWrite(13, ledState);
+	}
 	Serial.print("Time: ");
-	time=millis();
+	time = millis();
 	//prints time since program started
 	Serial.println(time);
 	// wait a second so as not to send massive amounts of data
 	//delay(100);
-	}
+}
