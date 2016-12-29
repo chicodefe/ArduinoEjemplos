871ad7b06a1fdab828d3de2cf6b4174e1555a9ac
chicodefe <chicodefe@gmail.com>
2016-12-28 20:41:33 -0600

    Add files via upload

--- SensorBasicoFlujo.ino
+++ SensorBasicoFlujo.ino
@@ -13,64 +13,63 @@ Author:	Chicodefe
 #include <LiquidCrystal_I2C.h>
 
 //Parametros Display
-LiquidCrystal_I2C lcd(0x3f,2,1,0,4,5,6,7,3,POSITIVE); // Set the LCD I2C address
+LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // Set the LCD I2C address
 
 volatile long NumPulsos; //variable para la cantidad de pulsos recibidos
-int PinSensor=2;    //Sensor conectado en el pin 2
-int tiempoReset=0;
+int PinSensor = 2;    //Sensor conectado en el pin 2
+int tiempoReset = 0;
 
 void ContarPulsos()//---Función que se ejecuta en interrupción
-	{
+{
 	NumPulsos++;  //incrementamos la variable de pulsos
-	}
+}
 
 int ObtenerFrecuencia()//---Función para obtener frecuencia de los pulsos
-	{
+{
 	int frecuencia;
-	NumPulsos=0;   //Ponemos a 0 el número de pulsos
+	NumPulsos = 0;   //Ponemos a 0 el número de pulsos
 	interrupts();    //Habilitamos las interrupciones
 	delay(1000);   //muestra de 1 segundo
 	noInterrupts(); //Desabilitamos las interrupciones
-	frecuencia=NumPulsos; //Hz(pulsos por segundo)
+	frecuencia = NumPulsos; //Hz(pulsos por segundo)
 	return frecuencia;
-	}
+}
 
 void setup()// the setup function runs once when you press reset or power the board
-	{
+{
 
 	//Parametros Pines
-	pinMode(PinSensor,INPUT); // sets the digital pin 2 as input
+	pinMode(PinSensor, INPUT); // sets the digital pin 2 as input
 
 	//Parametros Serial
 	Serial.begin(9600);// initialize serial communication at 9600 bits per second
 
-	attachInterrupt(digitalPinToInterrupt(2),ContarPulsos,CHANGE); //(Interrupcion 0(Pin2),funcion,Flanco de subida)
-	}
+	attachInterrupt(digitalPinToInterrupt(2), ContarPulsos, CHANGE); //(Interrupcion 0(Pin2),funcion,Flanco de subida)
+}
 
 void loop()// the loop function runs over and over again until power down or reset
-	{
-	int frecuencia=ObtenerFrecuencia(); //obtenemos la Frecuencia de los pulsos en Hz
+{
+	int frecuencia = ObtenerFrecuencia(); //obtenemos la Frecuencia de los pulsos en Hz
 
-
-	Serial.print("Pulsos por Segundo: ");//-----Enviamos por el puerto serie---------------
+	Serial.print("Pulsos por Segundo: ");//Enviamos por el puerto serie
 	Serial.println(frecuencia);
 
-	if(frecuencia!=0)
-		{
-		tiempoReset=5;
-		}
-	else if(frecuencia==0&&tiempoReset>0)
-		{
-		tiempoReset=--tiempoReset;
-		}
+	if (frecuencia != 0)//obtenemos la Frecuencia de los pulsos en Hz
+	{
+		tiempoReset = 5;//fijamos en 5 loops el tiempo
+	}
+	else if (frecuencia == 0 && tiempoReset > 0)
+	{
+		tiempoReset = --tiempoReset;//quitamos 1 loop al tiempo
+	}
 
-	if(tiempoReset>0)
-		{
+	if (tiempoReset > 0)
+	{//Codigo cuando se detecta flujo
 		Serial.print("Activado ");
 		Serial.println(tiempoReset);
-		}
+	}
 	else
-		{Serial.println("Inactivo ");
-	
+	{//Codigo cuando no se detecta flujo
+		Serial.println("Inactivo ");
 	}
-	}
+}
